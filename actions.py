7e38ea9a2e030f34293a773a503023b0d48e091c
Dustin Dannenhauer <dannenhauerdustin@gmail.com>
2020-05-09 17:14:33 -0400

    almost all commands and their corresponding execution messages are encoded

--- actions.py
+++ actions.py
@@ -40,8 +40,23 @@ class Command(Enum):
     INTERLEVEL_TRAVEL = 12
     FIND_ITEMS = 13
     SET_WAYPOINT = 14
-    LONG_WALK = 15
-    ATTACK_WITHOUT_MOVE = 16
+    LONG_WALK_SW = 15
+    LONG_WALK_S = 16
+    LONG_WALK_SE = 17
+    LONG_WALK_W = 18
+    LONG_WALK_E = 19
+    LONG_WALK_NW = 20
+    LONG_WALK_N = 21
+    LONG_WALK_NE = 22
+
+    ATTACK_WITHOUT_MOVE_SW = 15
+    ATTACK_WITHOUT_MOVE_S = 16
+    ATTACK_WITHOUT_MOVE_SE = 17
+    ATTACK_WITHOUT_MOVE_W = 18
+    ATTACK_WITHOUT_MOVE_E = 19
+    ATTACK_WITHOUT_MOVE_NW = 20
+    ATTACK_WITHOUT_MOVE_N = 21
+    ATTACK_WITHOUT_MOVE_NE = 22
 
     #  Autofight
     AUTO_FIGHT = 17
@@ -63,6 +78,7 @@ class Command(Enum):
     PUT_ON_JEWELLERY = 31
     REMOVE_JEWELLERY = 32
     EVOKE = 33
+    SELECT_ITEM_TO_EVOKE = 34
     MEMORISE = 34
     COUNT_GOLD = 35
 
@@ -72,7 +88,6 @@ class Command(Enum):
     CAST_SPELL_NO_MATTER_WHAT = 38
     LIST_ALL_SPELLS = 39
     TELL_ALLIES = 40
-    SHOUT_ALLIES = 41
     REDO_PREVIOUS_COMMAND = 42
 
     #  Game Saving and Quitting
@@ -104,19 +119,16 @@ class Command(Enum):
     TOGGLE_VIEW_LAYERS = 64
     SHOW_DUNGEON_OVERVIEW = 65
     TOGGLE_AUTO_PICKUP = 66
-    TOGGLE_MUTE_SOUND_EFFECTS = 67
     SET_TRAVEL_SPEED_TO_CLOSEST_ALLY = 68
 
     #  Item Interaction (Inventory)
     SHOW_INVENTORY_LIST = 69
     INSCRIBE_ITEM = 70
-    FIRE_NEXT_APPROPRIATE_ITEM = 71
-    SELECT_ITEM_AND_FIRE = 72
 
     # Item Interaction (floor)
     PICKUP_ITEM = 73
+    SELECT_ITEM_FOR_PICKUP = 73
     DROP_ITEM = 74
-    DROP_EXACT_NUMBER_OF_ITEMS = 75
     DROP_LAST_ITEMS_PICKED_UP = 76
 
     #  Additional Actions
@@ -132,33 +144,120 @@ class Action:
     This class represents an action that the agent can take.
     """
 
-    command_to_msg = \
-        {Command.MOVE_OR_ATTACK_N: {'msg': 'key', "keycode": -254},
-         Command.MOVE_OR_ATTACK_S: {'msg': 'key', "keycode": -253},
-         Command.MOVE_OR_ATTACK_E: {'msg': 'key', "keycode": -251},
-         Command.MOVE_OR_ATTACK_W: {'msg': 'key', "keycode": -252},
-         Command.MOVE_OR_ATTACK_NW: {'msg': 'key', "keycode": -1007},
-         Command.MOVE_OR_ATTACK_SW: {'msg': 'key', "keycode": -1001},
-         Command.MOVE_OR_ATTACK_SE: {'msg': 'key', "keycode": -1003},
-         Command.MOVE_OR_ATTACK_NE: {'msg': 'key', "keycode": -1009},
+    command_to_msg = {
+        Command.MOVE_OR_ATTACK_N: {'msg': 'key', "keycode": -254},
+        Command.MOVE_OR_ATTACK_S: {'msg': 'key', "keycode": -253},
+        Command.MOVE_OR_ATTACK_E: {'msg': 'key', "keycode": -251},
+        Command.MOVE_OR_ATTACK_W: {'msg': 'key', "keycode": -252},
+        Command.MOVE_OR_ATTACK_NW: {'msg': 'key', "keycode": -249},
+        Command.MOVE_OR_ATTACK_SW: {'msg': 'key', "keycode": -248},
+        Command.MOVE_OR_ATTACK_SE: {'msg': 'key', "keycode": -245},
+        Command.MOVE_OR_ATTACK_NE: {'msg': 'key', "keycode": -246},
 
-         # todo figure out message structure for the rest of the commands and add them here
+        Command.REST_AND_LONG_WAIT: {'text': '5', 'msg': 'input'},
+        Command.WAIT_1_TURN: {'text': '.', 'msg': 'input'},
 
-         Command.TRAVEL_STAIRCASE_UP: {'text': '<', 'msg': 'input'},
-         Command.TRAVEL_STAIRCASE_DOWN: {'text': '<', 'msg': 'input'},
-         Command.RESPOND_YES_TO_PROMPT: {'text': 'Y', 'msg': 'input'},
-         Command.RESPOND_NO_TO_PROMPT: {'text': 'N', 'msg': 'input'},
-         Command.CLEAR_GAME_TEXT_MESSAGES: {'text': '\r', 'msg': 'input'},
-         Command.QUAFF: {'text': 'q', 'msg': 'input'},
-         Command.EAT: {'text': 'e', 'msg': 'input'},
-         Command.PICKUP_ITEM: {'text': 'g', 'msg': 'input'},
-         Command.REST_AND_LONG_WAIT: {'text': '5', 'msg': 'input'},
-         Command.AUTO_EXPLORE: {'text': 'o', 'msg': 'input'},
-         Command.AUTO_FIGHT: {'msg': 'key', 'keycode': 9},
-         Command.SHOW_ABILITIES_AND_MUTATIONS: {'text': 'a', 'msg': 'input'},
-         Command.SHOW_INVENTORY_LIST: {'text': 'i', 'msg': 'input'},
-         Command.EXIT_MENU: {'msg': 'key', 'keycode': 27},
-         Command.SHOW_PREVIOUS_GAME_MESSAGES: {'msg': 'key', 'keycode': 16}}
+        Command.AUTO_EXPLORE: {'text': 'o', 'msg': 'input'},
+        Command.INTERLEVEL_TRAVEL: {'text': 'G', 'msg': 'input'},
+        Command.FIND_ITEMS: {'msg': 'key', "keycode": 6},
+         #  Todo - see github issue 9
+         # Command.SET_WAYPOINT: {...}
+
+        Command.LONG_WALK_N: {'msg': 'key', "keycode": -243},
+        Command.LONG_WALK_S: {'msg': 'key', "keycode": -242},
+        Command.LONG_WALK_E: {'msg': 'key', "keycode": -240},
+        Command.LONG_WALK_W: {'msg': 'key', "keycode": -241},
+        Command.LONG_WALK_NW: {'msg': 'key', "keycode": -238},
+        Command.LONG_WALK_NE: {'msg': 'key', "keycode": -235},
+        Command.LONG_WALK_SW: {'msg': 'key', "keycode": -237},
+        Command.LONG_WALK_SE: {'msg': 'key', "keycode": -234},
+
+        Command.ATTACK_WITHOUT_MOVE_N: {'msg': 'key', "keycode": -232},
+        Command.ATTACK_WITHOUT_MOVE_S: {'msg': 'key', "keycode": -231},
+        Command.ATTACK_WITHOUT_MOVE_E: {'msg': 'key', "keycode": -229},
+        Command.ATTACK_WITHOUT_MOVE_W: {'msg': 'key', "keycode": -230},
+        Command.ATTACK_WITHOUT_MOVE_NW: {'msg': 'key', "keycode": -227},
+        Command.ATTACK_WITHOUT_MOVE_SW: {'msg': 'key', "keycode": -226},
+
+         #  Todo - similar issue to github issue 9
+         # Command.ATTACK_WITHOUT_MOVE_NE: {'msg': 'key', "keycode": },
+
+         #  Todo - similar issue to github issue 9
+         # Command.ATTACK_WITHOUT_MOVE_SE: {'msg': 'key', "keycode": },
+
+        Command.AUTO_FIGHT: {'msg': 'key', 'keycode': 9},
+
+         #  Todo - similar issue to github issue 9
+         # Command.AUTO_FIGHT_WITHOUT_MOVE:
+
+        Command.WIELD_HAND_WEAPON: {'text': 'w', 'msg': 'input'},
+        Command.QUIVER_MISSILE: {'text': 'Q', 'msg': 'input'},
+        Command.FIRE_MISSILE: {'text': 'f', 'msg': 'input'},
+        Command.SELECT_MISSILE_AND_FIRE: {'text': 'F', 'msg': 'input'},
+        Command.CYCLE_MISSILE_FORWARD: {'text': '(', 'msg': 'input'},
+        Command.CYCLE_MISSILE_BACKWARD: {'text': ')', 'msg': 'input'},
+        Command.WEAR_ARMOUR: {'text': 'W', 'msg': 'input'},
+        Command.TAKE_OFF_ARMOUR: {'text': 'T', 'msg': 'input'},
+        Command.CHOP_CORPSE: {'text': 'c', 'msg': 'input'},
+        Command.EAT: {'text': 'e', 'msg': 'input'},
+        Command.QUAFF: {'text': 'q', 'msg': 'input'},
+        Command.READ: {'text': 'r', 'msg': 'input'},
+        Command.PUT_ON_JEWELLERY: {'text': 'P', 'msg': 'input'},
+        Command.REMOVE_JEWELLERY: {'text': 'R', 'msg': 'input'},
+        Command.EVOKE: {'text': 'v', 'msg': 'input'},
+        Command.SELECT_ITEM_TO_EVOKE: {'text': 'V', 'msg': 'input'},
+        Command.MEMORISE: {'text': 'M', 'msg': 'input'},
+        Command.COUNT_GOLD: {'text': '$', 'msg': 'input'},
+
+        Command.USE_SPECIAL_ABILITY: {'text': 'a', 'msg': 'input'},
+        Command.CAST_SPELL_ABORT_WITHOUT_TARGETS: {'text': 'z', 'msg': 'input'},
+        Command.CAST_SPELL_NO_MATTER_WHAT: {'text': 'Z', 'msg': 'input'},
+        Command.LIST_ALL_SPELLS: {'text': 'I', 'msg': 'input'},
+        Command.TELL_ALLIES: {'text': 'I', 'msg': 'input'},
+        Command.REDO_PREVIOUS_COMMAND: {'text': '`', 'msg': 'input'},
+
+        Command.SAVE_GAME_AND_EXIT: {'text': 'S', 'msg': 'input'},
+        Command.SAVE_AND_EXIT_WITHOUT_QUERY: {'msg': 'key', "keycode": 19},
+        Command.ABANDON_CURRENT_CHARACTER_AND_QUIT_GAME: {'msg': 'key', "keycode": 17},
+
+        Command.DISPLAY_CHARACTER_STATUS: {'text': '@', 'msg': 'input'},
+        Command.SHOW_SKILL_SCREEN: {'text': 'm', 'msg': 'input'},
+        Command.CHARACTER_OVERVIEW: {'text': '%', 'msg': 'input'},
+        Command.SHOW_RELIGION_SCREEN: {'text': '^', 'msg': 'input'},
+        Command.SHOW_ABILITIES_AND_MUTATIONS: {'text': 'A', 'msg': 'input'},
+        Command.SHOW_ITEM_KNOWLEDGE: {'text': '\\', 'msg': 'input'},
+        Command.SHOW_RUNES_COLLECTED: {'text': '}', 'msg': 'input'},
+        Command.DISPLAY_WORN_ARMOUR: {'text': '[', 'msg': 'input'},
+        Command.DISPLAY_WORN_JEWELLERY: {'text': '\"', 'msg': 'input'},
+        Command.DISPLAY_EXPERIENCE_INFO: {'text': 'E', 'msg': 'input'},
+
+        Command.OPEN_DOOR: {'text': 'O', 'msg': 'input'},
+        Command.CLOSE_DOOR: {'text': 'C', 'msg': 'input'},
+        Command.TRAVEL_STAIRCASE_UP: {'text': '<', 'msg': 'input'},
+        Command.TRAVEL_STAIRCASE_DOWN: {'text': '<', 'msg': 'input'},
+        Command.EXAMINE_CURRENT_TILE_PICKUP_PART_OF_SINGLE_STACK: {'text': ';', 'msg': 'input'},
+        Command.EXAMINE_SURROUNDINGS_AND_TARGETS: {'text': 'x', 'msg': 'input'},
+        Command.EXAMINE_LEVEL_MAP: {'text': 'X', 'msg': 'input'},
+        Command.LIST_MONSTERS_ITEMS_FEATURES_IN_VIEW: {'msg': 'key', "keycode": 24},
+        Command.TOGGLE_VIEW_LAYERS: {'text': '|', 'msg': 'input'},
+        Command.SHOW_DUNGEON_OVERVIEW: {'msg': 'key', "keycode": 15},
+        Command.TOGGLE_AUTO_PICKUP: {'msg': 'key', "keycode": 1},
+        Command.SET_TRAVEL_SPEED_TO_CLOSEST_ALLY: {'msg': 'key', "keycode": 5},
+
+        Command.SHOW_INVENTORY_LIST: {'text': 'i', 'msg': 'input'},
+        Command.INSCRIBE_ITEM: {'msg': 'input', 'data': [123]},
+
+        Command.PICKUP_ITEM: {'text': 'g', 'msg': 'input'},
+        Command.SELECT_ITEM_FOR_PICKUP: {'text': ',', 'msg': 'input'},
+        Command.DROP_ITEM: {'text': 'd', 'msg': 'input'},
+        Command.DROP_LAST_ITEMS_PICKED_UP: {'text': 'D', 'msg': 'input'},
+
+        Command.EXIT_MENU: {'msg': 'key', 'keycode': 27},
+        Command.SHOW_PREVIOUS_GAME_MESSAGES: {'msg': 'key', 'keycode': 16},
+        Command.RESPOND_YES_TO_PROMPT: {'text': 'Y', 'msg': 'input'},
+        Command.RESPOND_NO_TO_PROMPT: {'text': 'N', 'msg': 'input'},
+        Command.CLEAR_GAME_TEXT_MESSAGES: {'text': '\r', 'msg': 'input'},
+        }
 
     @staticmethod
     def get_execution_repr(self, command: Command):
